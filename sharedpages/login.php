20e4e365ad04b90d4b5f277793aa52745ace06bf
esraa <esraafahmy03@gmail.com>
2021-08-23 22:29:46 +0200

    add header and footer

--- sharedpages/login.php
+++ sharedpages/login.php
@@ -63,37 +63,9 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 
     }
 }
-//  require 'header.php';
+ require '../shared components/header.php';
 
 ?>
-
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="UTF-8">
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <title>Sign In | Bootstrap Based Admin Template - Material Design</title>
-    <!-- Favicon-->
-    <link rel="icon" href="favicon.ico" type="image/x-icon">
-
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700&subset=latin,cyrillic-ext" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" type="text/css">
-
-    <!-- Bootstrap Core Css -->
-    <link href="plugins/bootstrap/css/bootstrap.css" rel="stylesheet">
-
-    <!-- Waves Effect Css -->
-    <link href="plugins/node-waves/waves.css" rel="stylesheet" />
-
-    <!-- Animation Css -->
-    <link href="plugins/animate-css/animate.css" rel="stylesheet" />
-
-    <!-- Custom Css -->
-    <link href="css/style.css" rel="stylesheet">
-</head>
-
 <body class="login-page">
     <div class="login-box">
         <div class="logo">
@@ -102,7 +74,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
         </div>
         <div class="card">
             <div class="body">
-                <form id="sign_in" method="POST">
+                <form id="sign_in" method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
                     <div class="msg">Sign in to start your session</div>
                     <div class="input-group">
                         <span class="input-group-addon">
@@ -142,21 +114,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
         </div>
     </div>
 
-    <!-- Jquery Core Js -->
-    <script src="plugins/jquery/jquery.min.js"></script>
+<?php 
 
-    <!-- Bootstrap Core Js -->
-    <script src="plugins/bootstrap/js/bootstrap.js"></script>
-
-    <!-- Waves Effect Plugin Js -->
-    <script src="plugins/node-waves/waves.js"></script>
-
-    <!-- Validation Plugin Js -->
-    <script src="plugins/jquery-validation/jquery.validate.js"></script>
-
-    <!-- Custom Js -->
-    <script src="js/admin.js"></script>
-    <script src="js/pages/examples/sign-in.js"></script>
-</body>
-
-</html>
+require '../shared components/footer.php';
+?>
